d508028029a4bd8d9ea1fde75791956b5afaaec0
Hehe_Harry <sreeharikumarreddyk@gmail.com>
2025-05-29 15:17:59 +0530

    [Sreehari K] Implemented JUnit Testing and Added Logger

---
+++ src/main/java/com/supplytracker/dto/CheckpointDto.java
@@ -0,0 +1,23 @@
+package com.supplytracker.dto;
+
+import com.supplytracker.entity.Status;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.time.LocalDateTime;
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CheckpointDTO{
+	@NotNull(message ="Shipment ID is required")
+	private Long shipmentId;
+	@NotNull(message ="Location is required")
+	@Size(min = 2, max=100, message ="Location must be between 2 and 100 characters")
+	private String location;
+	@NotNull(message ="Status is required")
+	private Status status;
+	private LocalDateTime timestamp;
+	
+}
