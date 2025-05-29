c8070baa8e06d5694b7cee575f3841b45ea8fb18
ns7237 <ns7237@srmist.edu.in>
2025-05-29 15:47:32 +0530

    [sindhu] implemented testing for Checkpoints and Event logs and added loggers in service and controller layers

--- src/main/java/com/supplytracker/controller/CheckpointLogController.java
+++ src/main/java/com/supplytracker/controller/CheckpointLogController.java
@@ -9,12 +9,13 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-
+import org.slf4j.*;
 
 @RestController
 @RequestMapping("/api/checkpoints")
 @RequiredArgsConstructor
 public class CheckpointLogController {
+	private static final Logger logger = LoggerFactory.getLogger(ChekpointLogController.class);
 
 
 	@Autowired
@@ -23,14 +24,18 @@ public class CheckpointLogController {
 
 	@PostMapping
 	public ResponseEntity<CheckpointDto>addCheckpoint(@Valid @RequestBody CheckpointDto request){
+		logger.info("Received request to add checkpoint: {}", request);
 		CheckpointDto message = service.addCheckpoint(request);
+		logger.info("Checkpoint added successfully for shipmentId {}" , request.getShipmentId());
 		return ResponseEntity.ok(message);
 	}
 	
 	@GetMapping("/shipment/{shipmentId}")
 
 	public ResponseEntity<List<CheckpointDto>> getLogForShipment(@PathVariable Long shipmentId){
+		logger.info("Fetching checkpoint logs for shipmentId {}", shipmentId);
 		List<CheckpointDto> logs = service.getCheckpointByShipment(shipmentId);
+		logger.info("Retrieved {} logs for shipmentId {}", logs.size(), shipmentId);
 		return ResponseEntity.ok(logs);
 	}
 }
